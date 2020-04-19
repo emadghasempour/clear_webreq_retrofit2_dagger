b3aa64dfe931b63e1c613ade39ddb09406f4af81
emad <ghasempour.e@gmail.com>
2020-04-19 18:27:49 +0430

    tests basic config

---
+++ app/src/main/java/com/example/getdata_web_dagger/di/products/MocKAppModule.java
@@ -0,0 +1,10 @@
+package com.example.getdata_web_dagger.di.products;
+
+import com.example.getdata_web_dagger.di.AppModule;
+
+import dagger.Module;
+
+@Module
+public class MocKAppModule extends AppModule{
+    
+}
--- app/src/test/java/com/example/getdata_web_dagger/Example.java
+++ app/src/test/java/com/example/getdata_web_dagger/Example.java
@@ -1,7 +1,36 @@
 package com.example.getdata_web_dagger;
 
+import com.example.getdata_web_dagger.features.products_list.datasource.NetworkDataSource;
+import com.example.getdata_web_dagger.features.products_list.datasource.ProductsApi;
+import com.example.getdata_web_dagger.features.products_list.model.Product;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Mockito.*;
+
 import org.junit.runner.RunWith;
 
-public class Example {
+import java.util.List;
 
+import javax.inject.Inject;
+
+import io.reactivex.Flowable;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class Example {
+    NetworkDataSource mockNetworkDataSource= mock(NetworkDataSource.class);
+    @Inject ProductsApi mockProductsApi;
+
+    @Test
+    public void checkNetwork(){
+        when(mockNetworkDataSource.loadProductsFromWeb()).thenReturn(null);
+        Flowable<List<Product>> arr = mockNetworkDataSource.loadProductsFromWeb();
+        assertEquals(null,arr);
+        //verify(mockProductsApi.getUser());
+    }
 }
